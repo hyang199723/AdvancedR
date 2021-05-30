1fdbdbd509bdca72bd7afc68f7acce623789bcf7
hyang23 <hyang23@ncsu.edu>
2021-05-30 11:28:30 -0400

    cpp fix

--- Rcpp/sourceC.cpp
+++ Rcpp/sourceC.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include <Rcpp.h>
-using namespace Rcpp;
 
-using namespace std;
+#include<iostream>
 
+//Single line comment
+
+//This is where the execution of program begins
 int main()
 {
+   // displays Hello World! on screen
+   std::cout<<"Hello World!";
+   return 0;
 }
